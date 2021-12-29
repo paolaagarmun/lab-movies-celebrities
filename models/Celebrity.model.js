2d7cdce5e3afe4d1edfc8b4537bdc03d980a0350
paolaagarmun <paola.a.garciamunoz@gmail.com>
2021-12-29 12:10:41 -0500

    added celebrity model

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,16 @@
-//  Add your code here
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CelebrityModel = new Schema({
+    name: {
+        type: String
+    },
+    occupation: {
+        type: String
+    },
+    catchPhrase: {
+        type: String
+    }
+})
+
+module.exports = CelebrityModel
